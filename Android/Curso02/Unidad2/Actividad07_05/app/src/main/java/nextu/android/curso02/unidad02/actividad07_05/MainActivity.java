ab4445b8f4d3c9cd5327151b839ebb95278d98e0
Javier Galdámez <javier.galdamez@creativaconsultores.com>
2016-08-25 22:37:46 -0600

    :skull: Práctica completa de una aplicación en Adroid. - Construcción de la App. - Creación de APK. - Publicación del App.

---
+++ Android/Curso02/Unidad2/Actividad07_05/app/src/main/java/nextu/android/curso02/unidad02/actividad07_05/MainActivity.java
@@ -0,0 +1,33 @@
+package nextu.android.curso02.unidad02.actividad07_05;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText campoNumero1, campoNumero2, campoResultado;
+    private TextView tituloResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        campoNumero1 = (EditText)findViewById(R.id.campo_numero1);
+        campoNumero2 = (EditText)findViewById(R.id.campo_numero2);
+
+        tituloResultado = (TextView) findViewById(R.id.titulo_resultado);
+        campoResultado = (EditText)findViewById(R.id.campo_resultado);
+    }
+
+    public void botonMultiplicar(View vista) {
+        int numero1 = Integer.parseInt(campoNumero1.getText().toString());
+        int numero2 = Integer.parseInt(campoNumero2.getText().toString());
+        int resultado = numero1 * numero2;
+
+        tituloResultado.setText("Resultado:");
+        campoResultado.setText(String.valueOf(resultado));
+    }
+}
---
+++ Android/Curso02/Unidad2/Actividad07_05/app/src/test/java/nextu/android/curso02/unidad02/actividad07_05/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package nextu.android.curso02.unidad02.actividad07_05;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
